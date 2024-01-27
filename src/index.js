524fa625ba5e46ebdbd546bcbfbfdb949957d9a8
Danyil Stanislavov <stanislavov.danik@gmail.com>
2024-01-27 21:53:19 +0200

    Create fetch functions

--- src/index.js
+++ src/index.js
@@ -1,1 +1,32 @@
-console.log();
+import axios from 'axios';
+import fetchBreeds from './js/cat-api';
+import { fetchCatByBreed } from './js/cat-api';
+import { fetchById } from './js/cat-api';
+axios.defaults.headers.common['x-api-key'] =
+  'live_EoW54H3YGthvuy3FZOdNRzDKAo3oTN81nlkgj6hP3P53J8f2vlewIwzUUDCcTun6';
+
+const select = document.querySelector('.breed-select');
+const cardInfo = document.querySelector('.cat-info');
+
+fetchBreeds()
+  .then(cat => {
+    select.insertAdjacentHTML('beforeend', cat);
+    return cat;
+  })
+  .then(cat => {
+    select.onchange = () => {
+      cardInfo.innerHTML = '';
+      fetchById(select.value).then(elem => {
+        cardInfo.insertAdjacentHTML(
+          'beforeend',
+          `
+        <p>${elem.name}</p>
+        <p>${elem.description}</p>
+        `
+        );
+      });
+      fetchCatByBreed(select.value).then(elem => {
+        cardInfo.insertAdjacentHTML('afterbegin', elem);
+      });
+    };
+  });
---
+++ src/js/cat-api.js
@@ -0,0 +1,55 @@
+export default function fetchBreeds() {
+  return fetch('https://api.thecatapi.com/v1/breeds')
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(response.status);
+      }
+      return response.json();
+    })
+    .then(data => {
+      return data
+        .map(element => {
+          return `<option value="${element.id}">${element.name}</option>`;
+        })
+        .join('');
+    })
+    .catch(error => {
+      console.log('Error', error);
+    });
+}
+
+export function fetchCatByBreed(breedId) {
+  return fetch(
+    `https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`
+  )
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(response.status);
+      }
+      return response.json();
+    })
+    .then(data => {
+      return `<img src="${data[0].url}" width="600" heigth="400">`;
+    })
+    .catch(error => {
+      console.log('Error', error);
+    });
+}
+
+export function fetchById(breedId) {
+  return fetch('https://api.thecatapi.com/v1/breeds')
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(response.status);
+      }
+      return response.json();
+    })
+    .then(data => {
+      return data.find(cat => {
+        return cat.id === breedId;
+      });
+    })
+    .catch(error => {
+      console.log('Error', error);
+    });
+}
